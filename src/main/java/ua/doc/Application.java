05565e4456410043fcc32c019adb21e50c1061ed
Doc <4594400@gmail.con>
2023-02-17 15:07:49 +0200

    added app

---
+++ src/main/java/ua/doc/Application.java
@@ -0,0 +1,13 @@
+package ua.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
